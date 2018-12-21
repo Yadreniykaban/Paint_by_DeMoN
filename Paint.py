f9a7846ec7adc4508fc3927b1861d5081a5bbd57
Yadreniykaban <dmitrynastavshev@gmail.com>
2018-12-21 17:09:05 +0200

    Прога работает, можно рисовать, изменять размер кисти, менять цвет

--- Paint.py
+++ Paint.py
@@ -1,6 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QMenuBar, QMenu, QAction
-from PyQt5.QtGui import QImage, QIcon
+from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, QFileDialog
+from PyQt5.QtGui import QImage, QIcon, QPainter, QPen
 from PyQt5.QtCore import Qt, QPoint
 
 
@@ -25,46 +25,121 @@ class Window(QMainWindow):
         fileMenu = mainMenu.addMenu("File")
         brushMenu = mainMenu.addMenu("Brush size")
         brushColor = mainMenu.addMenu("Color")
+        geometryMenu = mainMenu.addMenu("Geometry")
 
         saveAction = QAction(QIcon("icons/box_green.png"), "Save", self)
         saveAction.setShortcut("Ctrl+S")
         fileMenu.addAction(saveAction)
+        saveAction.triggered.connect(self.save)
 
         clearAction = QAction(QIcon("icons/box_paint.png"), "Clear", self)
         clearAction.setShortcut("Ctrl+Z")
         fileMenu.addAction(clearAction)
+        clearAction.triggered.connect(self.clear)
 
         threepxAction = QAction(QIcon("icons/paint.png"), "3px", self)
-        threepxAction.setShortcut("Ctrl+T")
-        fileMenu.addAction(threepxAction)
+        threepxAction.setShortcut("Ctrl+K")
+        brushMenu.addAction(threepxAction)
+        threepxAction.triggered.connect(self.threePx)
 
         fivepxAction = QAction(QIcon("icons/paint.png"), "5px", self)
         fivepxAction.setShortcut("Ctrl+F")
-        fileMenu.addAction(fivepxAction)
+        brushMenu.addAction(fivepxAction)
+        fivepxAction.triggered.connect(self.fivePx)
 
         sevenpxAction = QAction(QIcon("icons/paint.png"), "7px", self)
         sevenpxAction.setShortcut("Ctrl+O")
-        fileMenu.addAction(sevenpxAction)
+        brushMenu.addAction(sevenpxAction)
+        sevenpxAction.triggered.connect(self.sevenPx)
 
         ninepxAction = QAction(QIcon("icons/paint.png"), "9px", self)
         ninepxAction.setShortcut("Ctrl+T")
-        fileMenu.addAction(ninepxAction)
+        brushMenu.addAction(ninepxAction)
+        ninepxAction.triggered.connect(self.ninePx)
 
         blackAction = QAction(QIcon("icons/paint.png"), "Black", self)
         blackAction.setShortcut("Ctrl+B")
         brushColor.addAction(blackAction)
+        blackAction.triggered.connect(self.black)
 
         redAction = QAction(QIcon("icons/box_red.png"), "Red", self)
         redAction.setShortcut("Ctrl+R")
         brushColor.addAction(redAction)
+        redAction.triggered.connect(self.red)
 
         greenAction = QAction(QIcon("icons/box_green.png"), "Green", self)
         greenAction.setShortcut("Ctrl+G")
         brushColor.addAction(greenAction)
+        greenAction.triggered.connect(self.green)
 
         yellowAction = QAction(QIcon("icons/box_yellow.png"), "Yellow", self)
-        yellowAction.setShortcut("Ctrl+R")
+        yellowAction.setShortcut("Ctrl+Y")
         brushColor.addAction(yellowAction)
+        yellowAction.triggered.connect(self.yellow)
+
+        blueAction = QAction(QIcon("icons/box_blue.png"), "Blue", self)
+        blueAction.setShortcut("Ctrl+P")
+        brushColor.addAction(blueAction)
+        blueAction.triggered.connect(self.blue)
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.drawing = True
+            self.lastPoint = event.pos()
+
+    def mouseMoveEvent(self, event):
+        if (event.buttons() and Qt.LeftButton) and self.drawing:
+            painter = QPainter(self.image)
+            painter.setPen(QPen(self.brushColor, self.brushSize, Qt.SolidLine, Qt.RoundCap, Qt.RoundJoin))
+            painter.drawLine(self.lastPoint, event.pos())
+            self.lastPoint = event.pos()
+            self.update()
+
+    def mouseReleaseEvent(self, event):
+        if event.button == Qt.LeftButton:
+            self.drawing = False
+
+    def paintEvent(self, event):
+        canvasPainter = QPainter(self)
+        canvasPainter.drawImage(self.rect(), self.image, self.image.rect())
+
+    def save(self):
+        filePath, _ = QFileDialog.getSaveFileName(self, "Save image", "",
+                                                  "PNG(*.png);;JPEG(*.jpg *.jpeg);; ALL Files(*.*)")
+        if filePath == "":
+            return
+        self.image.save(filePath)
+
+    def clear(self):
+        self.image.fill(Qt.white)
+        self.update()
+
+    def threePx(self):
+        self.brushSize = 3
+
+    def fivePx(self):
+        self.brushSize = 5
+
+    def sevenPx(self):
+        self.brushSize = 7
+
+    def ninePx(self):
+        self.brushSize = 9
+
+    def green(self):
+        self.brushColor = Qt.green
+
+    def yellow(self):
+        self.brushColor = Qt.yellow
+
+    def blue(self):
+        self.brushColor = Qt.blue
+
+    def black(self):
+        self.brushColor = Qt.black
+
+    def red(self):
+        self.brushColor = Qt.red
 
 
 if __name__ == '__main__':
